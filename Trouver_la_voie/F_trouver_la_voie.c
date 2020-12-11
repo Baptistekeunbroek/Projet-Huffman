7f7a97fe14c3630a1a78aa037933ac0856e0bd64
Thibault PELTIER <thibault.peltier@efrei.net>
2020-12-11 18:50:12 +0100

    modif

--- Trouver_la_voie/F_trouver_la_voie.c
+++ Trouver_la_voie/F_trouver_la_voie.c
@@ -1,14 +1,24 @@
-
+/*****************************************************************//**
+ * \file   Huffman
+ * \brief  Fonction pour trouver chrcher chemin 
+ * \n Example :
+ * \code{.c}
+ * chemin_valide = 1;
+ * \endcode
+ * \return verification si le symbole de la case ou  l'on pointe est la bonne.
+ * \author Thibault, Baptiste, Leonie, Camille, Gabriel
+ * \date   Decembre 2020
+ *********************************************************************/
 int F_trouver_la_voie(liste_HUFFMAN *l_huff,int chemin_valide ,char ccl,FILE *livre1_compr){
 
     if(ccl == l_huff->symbole ){
             chemin_valide = 1;
-    }//verification si le symbole de la case ou  l'on pointe est la bonne
+    }
 
     if(chemin_valide == 1){
             putc(l_huff->num_h, livre1_compr);
-            //printf("%d",l_huff->num_h);///pour afficher le code de huffman de chaque carac au cour de sa recherch
-            return chemin_valide;//porte de sortie
+            
+            return chemin_valide;
     }else{
 
         if(l_huff->gauche != NULL){
@@ -17,6 +27,6 @@ int F_trouver_la_voie(liste_HUFFMAN *l_huff,int chemin_valide ,char ccl,FILE *li
         if(l_huff->droite != NULL){
         chemin_valide = F_trouver_la_voie(l_huff->droite, chemin_valide ,ccl ,livre1_compr);}
 
-    }//fin du if de recherche et de recursivité
+    }
 
-}//fin F_trouver_la_voie
+}
