e02e7c0f64038c5bdfedeac9edb0c092d3a4c6a5
Baptistekeunbroek <73796568+Baptistekeunbroek@users.noreply.github.com>
2020-12-11 17:11:12 +0100

    Delete afficher_liste_huff.c

--- afficher_liste_huff.c
+++
@@ -1,8 +0,0 @@
-#include "fonction.h"
-void afficher_liste_huff(liste_HUFFMAN *l_huff){
-    while(l_huff != NULL){
-        printf("  %c:%d ",l_huff->symbole ,l_huff->poid );
-
-        l_huff=l_huff->suivant;
-    }//fin while
-}//fin afficher_liste_occur
